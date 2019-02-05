0b7c463c6eb5b9c8d3ebe7cdd2c43beec2bbfda4
Vojtěch Sázel <vojtech@sazel.cz>
2019-02-05 09:14:10 +0100

    Addressing PR comments

--- server/push-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#use TAG env. variable to create the container with the given tag
-TAG="${TAG:-latest}"
-
-CONTAINER="aerogear/voyager-server-example-task:$TAG"
-echo "Building docker container $CONTAINER"
-docker build -f Dockerfile -t "$CONTAINER" . && docker push "$CONTAINER"
