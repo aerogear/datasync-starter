8fb796f7938b221def627c48f6a61a526ff5df72
Davide <david3bizzarri@gmail.com>
2019-04-26 12:58:03 +0200

    wip: enable browserstack local

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,6 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+var browserstack = require('browserstack-local');
 
 exports.config = {
   seleniumAddress: 'http://hub.browserstack.com/wd/hub',
@@ -17,6 +18,8 @@ exports.config = {
     autoWebview: true,
     'browserstack.user': process.env.BROWSERSTACK_USER,
     'browserstack.key': process.env.BROWSERSTACK_KEY,
+    'browserstack.local' : true,
+    'browserstack.debug': true,
   },
   framework: 'jasmine',
   jasmineNodeOpts: {
@@ -29,5 +32,26 @@ exports.config = {
       project: 'e2e/tsconfig.e2e.json'
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  },
+
+  // Code to start browserstack local before start of test
+  beforeLaunch: function(){
+    console.log("Connecting local");
+    return new Promise(function(resolve, reject){
+      exports.bs_local = new browserstack.Local();
+      exports.bs_local.start({'key': process.env.BROWSERSTACK_KEY }, function(error) {
+        if (error) return reject(error);
+        console.log('Connected. Now testing...');
+
+        resolve();
+      });
+    });
+  },
+
+  // Code to stop browserstack local after end of test
+  afterLaunch: function(){
+    return new Promise(function(resolve, reject){
+      exports.bs_local.stop(resolve);
+    });
   }
 };
