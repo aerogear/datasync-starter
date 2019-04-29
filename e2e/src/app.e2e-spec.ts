3499fe53c9b9a7b0fa856524c175fcc4067483d0
Davide <david3bizzarri@gmail.com>
2019-04-29 16:17:23 +0200

    restore: aerogear logo test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,8 +1,7 @@
 import { element, by } from 'protractor';
 
 describe('App', () => {
-  it('AeroGear logo should be visible', async () => {
-    await element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-router-outlet/app-home/ion-header/ion-toolbar/ion-buttons')).click();
-    await element(by.xpath('/html/body/app-root/ion-app/ion-split-pane/ion-menu/ion-content/ion-list/ion-menu-toggle[2]/ion-item')).click();
+  it('AeroGear logo should be visible', () => {
+    expect(element(by.css('ion-img.logo')).isDisplayed()).toBeTruthy();
   });
 });
