5839776fdd1d14652dbcb99dba800316f425e169
Wojciech Trocki <wtrocki@redhat.com>
2019-06-26 20:45:58 +0100

    fix: remove altair playground

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -20,10 +20,19 @@ class Config {
     this.pushConfigPath = process.env.PUSH_CONFIG || path.resolve(__dirname, './push-config.json')
     this.pushConfig = readConfig(this.pushConfigPath)
 
-    this.altairConfig = {
-      endpointURL: '/graphql',
-      subscriptionsEndpoint: 'ws://localhost:4000/graphql',
-      initialQuery: fs.readFileSync(path.resolve(__dirname, './playground.gql'), 'utf8'),
+
+    this.playgroundConfig = {
+      settings: {
+        'editor.theme': 'light',
+        'editor.cursorShape': 'block'
+      },
+      tabs: [
+        {
+          endpoint: `/graphql`,
+          variables: {},
+          query: fs.readFileSync(path.resolve(__dirname, './playground.gql'), 'utf8')
+        }
+      ]
     }
   }
 }
