4dfa0a4a5334e0375d7ac86f1539965e5f13bc5b
Wojciech Trocki <wtrocki@redhat.com>
2020-03-04 12:27:30 +0000

    fix: Swap mongo implementation back to graphback package

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -9,7 +9,7 @@ import { Config } from './config/config';
 import { ApolloServer } from "apollo-server-express";
 import { Express } from "express";
 import { buildKeycloakApolloConfig } from './auth';
-import { createOffixMongoCRUDRuntimeContext } from './data/MongoDBDataProvider';
+import { createOffixMongoCRUDRuntimeContext} from "@graphback/runtime-mongo"
 
 /**
  * Creates Apollo server
