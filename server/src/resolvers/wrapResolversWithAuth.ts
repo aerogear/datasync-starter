b7c4e8a98c49db5613e87bfeb5e5c5469f3695b3
Dara Hayes <dara.s.hayes@gmail.com>
2020-04-28 16:38:20 +0100

    comments on auth wrapper implementation

--- server/src/resolvers/wrapResolversWithAuth.ts
+++ server/src/resolvers/wrapResolversWithAuth.ts
@@ -15,19 +15,27 @@ function wrapResolverWithAuth(resolverAuthInfo: any, resolveFn: any): any {
   return async function resolve(obj, args, context, info) {
     let isAuthorizedByRole = false;
     let isAuthorizedOwner = false;
+    
+    // perform the role checks
     if (resolverAuthInfo.roles && resolverAuthInfo.roles.length > 0) {
       isAuthorizedByRole = checkIsAuthorizedByRole(resolverAuthInfo.roles, context)
     }
+    // perform the owner auth checks
     if (resolverAuthInfo.ownerAuth) {
       const ownerField = resolverAuthInfo.ownerField
       const modelName = resolverAuthInfo.modelName
 
-      // if we're in a create type resolver then
-      // set the owner field as the currently logged in user's ID
+      // if we're in a create type resolver then the user is allowed to proceed
       if (isACreateResolver(info, modelName)) {
         isAuthorizedOwner = true
+        // set the owner field as the currently logged in user's ID
         args.input[ownerField] = args.input[ownerField] || context.kauth.accessToken.content.sub
       } else {
+        // fetch the data they're trying to modify and match the logged in user ID
+        // against the record they are trying to modify
+        // I see no other database agnostic/generic way to do this
+        // In the future, caching could be added on the CRUDService layer to help this specific case
+        // and performance in general
         const crudService = context[modelName]
         const result = await crudService.findBy({ id: args.input.id })
         const itemToUpdate = result[0]
@@ -37,9 +45,11 @@ function wrapResolverWithAuth(resolverAuthInfo: any, resolveFn: any): any {
       }
     }
     console.log(`resolvername: ${info.fieldName}, is authorized by role: ${isAuthorizedByRole}, is an authorized owner: ${isAuthorizedOwner}`)
+    // if either one is true then the user can proceed
     if (isAuthorizedByRole || isAuthorizedOwner) {
       return resolveFn(obj, args, context, info)
     }
+    // throw generic unauthorized error - good for security
     throw new Error(`User is not authorized.`)
   }
 }
