122349c6e8532a0a0a2af0ee5124f47f47562e3a
Wojciech Trocki <wtrocki@redhat.com>
2020-06-05 17:33:57 +0100

    fix: Enable to work with the application without keycloak configuration enabled

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -1,7 +1,7 @@
 import { Express } from "express";
 import { config } from './config/config';
 
-import { 
+import {
     KeycloakTypeDefs,
     KeycloakSchemaDirectives,
     KeycloakSubscriptionContext,
@@ -13,55 +13,50 @@ const session = require('express-session')
 const Keycloak = require('keycloak-connect')
 
 export function buildKeycloakApolloConfig(app: Express, apolloConfig: any) {
-    if (config.keycloakConfig) {
-        const graphqlPath = `/graphql`;
-        console.log("Using keycloak configuration")
-        
-        const memoryStore = new session.MemoryStore()	      
-        app.use(session({	
-            secret: process.env.SESSION_SECRET_STRING || 'this should be a long secret',	
-            resave: false,	
-            saveUninitialized: true,	
-            store: memoryStore	
-        }))	
+    const graphqlPath = `/graphql`;
+    console.log("Using keycloak configuration")
 
-        const keycloak = new Keycloak({	
-            store: memoryStore	
-        }, config.keycloakConfig);
-        const keycloakSubscriptionHandler = new KeycloakSubscriptionHandler({ keycloak })
+    const memoryStore = new session.MemoryStore()
+    app.use(session({
+        secret: process.env.SESSION_SECRET_STRING || 'this should be a long secret',
+        resave: false,
+        saveUninitialized: true,
+        store: memoryStore
+    }))
 
-        app.use(keycloak.middleware())
+    const keycloak = new Keycloak({
+        store: memoryStore
+    }, config.keycloakConfig);
+    const keycloakSubscriptionHandler = new KeycloakSubscriptionHandler({ keycloak })
 
-        app.use(graphqlPath, keycloak.protect());
+    app.use(keycloak.middleware())
 
-        return {
-            typeDefs: [KeycloakTypeDefs, apolloConfig.typeDefs], // 1. Add the Keycloak Type Defs
-            schemaDirectives: KeycloakSchemaDirectives, 
-            resolvers: apolloConfig.resolvers,
-            playground: apolloConfig.playground,
-            path: graphqlPath,
-            context: ({ req }) => {
+    app.use(graphqlPath, keycloak.protect());
+
+    return {
+        typeDefs: [KeycloakTypeDefs, apolloConfig.typeDefs], // 1. Add the Keycloak Type Defs
+        schemaDirectives: KeycloakSchemaDirectives,
+        resolvers: apolloConfig.resolvers,
+        playground: apolloConfig.playground,
+        path: graphqlPath,
+        context: ({ req }) => {
+            return {
+                ...apolloConfig.context,
+                kauth: new KeycloakContext({ req }) // 3. add the KeycloakContext to `kauth`
+            }
+        },
+        subscriptions: {
+            onConnect: async (connectionParams, websocket, connectionContext) => {
+                const token = await keycloakSubscriptionHandler.onSubscriptionConnect(connectionParams)
+                if (!token) {
+                    throw new Error("Cannot build keycloak token. Connection will be terminated")
+                }
                 return {
                     ...apolloConfig.context,
-                    kauth: new KeycloakContext({ req }) // 3. add the KeycloakContext to `kauth`
+                    kauth: new KeycloakSubscriptionContext(token)
                 }
-            },
-            subscriptions: {
-                onConnect: async (connectionParams, websocket, connectionContext) => {
-                    const token = await keycloakSubscriptionHandler.onSubscriptionConnect(connectionParams)
-                    if (!token) {
-                        throw new Error("Cannot build keycloak token. Connection will be terminated")
-                    }
-                    return {
-                        ...apolloConfig.context,
-                        kauth: new KeycloakSubscriptionContext(token)
-                    }
-                }
-            },
-        }
-    } else {
-        console.log("Keycloak not configured. Auth will be disabled");
-        return apolloConfig;
+            }
+        },
     }
 }
 
